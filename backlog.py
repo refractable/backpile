5ef13a4e13a45d6d4e9d16270ae9a7e97ac28b0b
refractable <brianguerrero246@gmail.com>
2025-11-24 16:08:02 -0500

    Added formatting via rich for library

--- backlog.py
+++ backlog.py
@@ -1,11 +1,15 @@
 import requests
 import json
+from rich.console import Console
+from rich.table import Table
 
 with open("config.json") as f:
     config = json.load(f)
 
+
 API_KEY = config["API_KEY"]
 STEAM_ID = config["STEAM_ID"]
+
 
 url = (
     f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/"
@@ -15,8 +19,20 @@ url = (
 
 response = requests.get(url)
 data = response.json()
+console = Console()
+
+
+table = table = Table(title="My Steam Backlog")
+table.add_column("Game", justify="left", style="cyan", no_wrap=False)
+table.add_column("Playtime", justify="right", style="green")
+
 
 games = data["response"]["games"]
 
+
 for game in games:
-    print(f"{game['name']} ({game['playtime_forever']}) minutes played")
+    hours = game["playtime_forever"] / 60
+    table.add_row(game["name"], f"{hours:.2f} hours")
+
+
+console.print(table)
